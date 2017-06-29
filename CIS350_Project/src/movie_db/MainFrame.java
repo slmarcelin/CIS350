91a93e97c945fb98c2c6eaf6eb0cfcfb0eb7c56b
willisoa <willisoa@C-5319.gentex.com>
2017-06-28 20:34:57 -0400

    Added a label

--- CIS350_Project/src/movie_db/MainFrame.java
+++ CIS350_Project/src/movie_db/MainFrame.java
@@ -18,10 +18,20 @@ import com.jgoodies.forms.layout.RowSpec;
 import java.awt.FlowLayout;
 import javax.swing.JButton;
 import java.awt.Button;
+import javax.swing.JTabbedPane;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class MainFrame extends JFrame {
 	private final Action action = new SwingAction();
 	private final Action action_1 = new SwingAction_1();
+	
+	JMenuBar menuBar = new JMenuBar();
+	JMenuItem mntmNewMenuItem_1 = new JMenuItem("Menu Item 1");
+	JButton btnNewButton = new JButton("Click Me!");
+	JLabel lblNewLabel = new JLabel("");
+	
+	int count = 0;
 
 	/**
 	 * Launch the application.
@@ -46,15 +56,15 @@ public class MainFrame extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		
-		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Menu Item 1");
 		menuBar.add(mntmNewMenuItem_1);
 		
-		JButton btnNewButton = new JButton("Click Me!");
 		btnNewButton.setAction(action_1);
 		getContentPane().add(btnNewButton, BorderLayout.SOUTH);
+		
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		getContentPane().add(lblNewLabel, BorderLayout.CENTER);
 	}
 
 	private class SwingAction extends AbstractAction {
@@ -73,6 +83,8 @@ public class MainFrame extends JFrame {
 		}
 		public void actionPerformed(ActionEvent e) {
 			System.out.println("Click Me! Clicked");
+			count++;
+			lblNewLabel.setText("Count : " + count);
 		}
 	}
 }
