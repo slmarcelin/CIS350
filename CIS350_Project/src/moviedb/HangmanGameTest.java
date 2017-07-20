2cbe6795d43f89d50b126e9ac5df3cda06dfa730
Zachary-Ash <zachary.j.ash@gmail.com>
2017-07-20 18:44:18 -0400

    Fixed hangman tests

--- CIS350_Project/src/moviedb/HangmanGameTest.java
+++ CIS350_Project/src/moviedb/HangmanGameTest.java
@@ -6,49 +6,44 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class HangmanGameTest {
-	private static final String TEST_WORD = "asglek:JLTJlj;lj"; 
+	private static final String TEST_WORD = "a;iuiwewljljlssxz"; 
     @Test
-    public void testGuessingCompleteWord() {
+    public void testBasics() {
         HangmanGame game = new HangmanGame(TEST_WORD);
 
-        game.isWithin(game.getWord());
     	assertEquals(game.getIncorrect(), 0);
-    	assertEquals(game.getState(), 2);
-    	assertEquals(game.getGuess(), game.getWord());
+    	assertTrue(game.getWord().equalsIgnoreCase(TEST_WORD));
+    	assertEquals(game.getState(), 0);
     }
     
+    @Test
     public void testGuessingAllLetters() {
         HangmanGame game = new HangmanGame(TEST_WORD);
-
-        assertEquals(game.getState(), 0);
         
         String nextLetter;
-        for(int i = 0; i < game.getWord().length() - 1; i++) {
-        	nextLetter = game.getWord().substring(i, i+1);
-        	game.isWithin(nextLetter);
+        for(int i = 0; i < game.getWord().length(); i++) {
         	assertEquals(game.getState(), 0);
         	assertEquals(game.getIncorrect(), 0);
+        	nextLetter = game.getWord().substring(i, i+1);
+        	game.isWithin(nextLetter);
         }
-        nextLetter = game.getWord().substring(game.getWord().length() - 1);
-    	game.isWithin(nextLetter);
     	assertEquals(game.getIncorrect(), 0);
     	assertEquals(game.getState(), 2);
     	assertEquals(game.getGuess(), game.getWord());
     }
     
+    @Test
     public void testGuessingWrong() {
         HangmanGame game = new HangmanGame(TEST_WORD);
     	
         String startGuess = game.getGuess();
         String wrong = game.getWord() + "X";
         
-        assertEquals(game.getState(), 0);
-        for(int i = 0; i < 5; i++) {
-        	game.isWithin(wrong);
+        for(int i = 0; i < 6; i++) {
         	assertEquals(game.getState(), 0);
         	assertEquals(game.getIncorrect(), i);
+        	game.isWithin(wrong);
         }
-    	game.isWithin(wrong);
     	assertEquals(game.getState(), 1);
     	assertEquals(game.getIncorrect(), 6);
     	assertEquals(game.getGuess(), startGuess);
