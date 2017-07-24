7d218025dab25618a989fb1b5d04dba9b4bb2207
marcelis <marcelis@B7-EE-CE.egr.gvsu.edu>
2017-07-24 12:31:57 -0400

    added more questions to trivia

--- CIS350_Project/src/moviedb/TriviaQuestion.java
+++ CIS350_Project/src/moviedb/TriviaQuestion.java
@@ -55,26 +55,41 @@ public class TriviaQuestion {
     public static TriviaQuestion generateRandomQuestion() {
         String[] setQuestions = new String[] {
                 "Brad Pitt plays General Glen McMahon\n"
-                        + "in which film directed by David Michod?",
+                       + "in which film directed by David Michod?",
                 "Which star plays Mitch Buchannon in\n"
                         + "the Baywatch movie released in 2017?",
                 "The character played by Hugh Jackman in"
                         + "nine of the ten X-Men movie franchise films?",
-                "He played 'Beast' in the film adapation of"
-                        + "the fairy tale 'Beauty and the Beast'?",
-                "Who always plays captain Jack Sparrow?"
+                "He played 'Beast' in the film adapation of "
+                        +"the fairy tale 'Beauty and the Beast'?",
+                "Who always plays captain Jack Sparrow?",               
+                 "In the Star Wars universe,"
+                             +"who is Luke Skywalker's mother?",
+                 "In the Lord of the Rings film series,"
+                              +"which actor plays the character of Saruman In the 2016,",
+                 "In the American fantasy adventure film 'The Jungle Book',"
+                                +"what is the name of the orphaned human boy?",
+                 "The name of the actress who plays Hermione Granger,"
+                                +"in the Harry Potter series of films?",
+                 "The name of the kleptomaniac monkey,"
+                                +"in the Disney movie 'Aladdin'?"
         };
         String[] setAnswers = new String[]{
                 "war machine",
                 "dwayne johnson",
                 "logan",
                 "dan stevens",
-                "johnny depp"
+                "johnny depp",
+                "padme amidala",
+                "christopher lee",
+                "mowgli",
+                "emma watson",
+                "abu"
         };
         TriviaQuestion q = new TriviaQuestion(
-                setQuestions[currQuestion], setAnswers[currQuestion]);
-        currQuestion++;
-        currQuestion %= 5;
+         setQuestions[currQuestion], setAnswers[currQuestion]);
+         currQuestion++;
+         currQuestion %=5;
         return q;
     }
 }
